e6d0f5d6437a8050dcb3f6fbd253227c096ccc8e
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-09-10 23:31:32 +0200

    all node modules

--- frontend/.next/dev/static/chunks/_219uq1s._.js
+++ frontend/.next/dev/static/chunks/_219uq1s._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0-n2ws6._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_next_dist_0dfntjm._.js"],
+    chunks: ["static/chunks/[root-of-the-server]__0-n2ws6._.css","static/chunks/node_modules_next_1iksayt._.js","static/chunks/src_components_shell_jsx_0nd7p94._.js","static/chunks/src_0cm117o._.js","static/chunks/src_components_states_jsx_1y-kldp._.js","static/chunks/src_12-wou9._.js","static/chunks/node_modules_recharts_es6_util_1m1grls._.js","static/chunks/node_modules_recharts_es6_cartesian_1p2idx-._.js","static/chunks/node_modules_recharts_es6_component_0uz9ds0._.js","static/chunks/node_modules_recharts_es6_state_04nuog5._.js","static/chunks/node_modules_recharts_es6_1q7nd_0._.js","static/chunks/node_modules_es-toolkit_dist_0w-std_._.js","static/chunks/node_modules_1tofqpt._.js"],
     source: "entry"
 });
